054c1404340b927aea1bbbe9e6aed3a2be1fafd5
shabane <m.mohamadshabane@gmail.com>
2023-04-23 03:07:27 +0330

    [feat](wreite database file)

--- outline/models.py
+++ outline/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    name = models.CharField(max_length=100)
+    max_size = models.IntegerField(default=3000)
+    usage = models.IntegerField(default=0)
+    enabled = models.BooleanField(default=True)
+    key = models.TextField()
+    birth_date = models.DateField(auto_now_add=True)
+    exp_date = models.DateField()
+
+
+class Server(models.Model):
+    certSha256 = models.TextField()
+    apiUrl = models.CharField(max_length=255)
