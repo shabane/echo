2326d86c75e00f2dae988292f9e52a946eb5ed9c
shabane <m.mohamadshabane@gmail.com>
2023-04-23 03:10:23 +0330

    [feat](write basic routing)

--- outline/urls.py
+++ outline/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import LinkViewSet, ServerViewSet
+
+
+router = DefaultRouter()
+router.register('users', LinkViewSet, basename='users')
+router.register('server', ServerViewSet, basename='server')
+
 
 urlpatterns = [
-    path('', views.home)
+    path('', include(router.urls))
 ]
