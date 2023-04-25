a4a00b31701e7f8fb191a5988920108c79a46199
shabane <m.mohamadshabane@gmail.com>
2023-04-26 02:18:49 +0330

    [feat](write a method to add new key with {name, limt}):
    	add two new method:
    		1. http_put method for requtst a put
    		2. method which use http_put for add new key to server

--- outline/core/outline.py
+++ outline/core/outline.py
@@ -18,6 +18,22 @@ class Outline():
         return res
     
     
+    @staticmethod
+    def __http_post(url: str, json_data: dict) -> requests.models.Response:
+        res = requests.post(url, json=json.dumps(json_data), verify=False)
+        if res.status_code != 201:
+            raise ConnectionAbortedError('could not connect to the ApiUrl(HTTP POST)')
+        return res
+        
+    
+    @staticmethod
+    def __http_put(url: str, json_data: dict={}, data: dict={}) -> requests.models.Response:
+        res = requests.put(url, json=json_data, data=data, verify=False)
+        if res.status_code != 204:
+            raise ConnectionAbortedError(f'{res.status_code}: could not connect to the ApiUrl(HTTP PUT)')
+        return res
+    
+    
     def __init__(self, apiUrl: str, certSha256: str) -> None:
         self.apiUrl = apiUrl
         self.certSha256 = certSha256
@@ -36,6 +52,17 @@ class Outline():
         """Lists the access keys
 
         Returns:
-            dict: 200 Lists og access keys(HTTP GET)
+            dict: 200, Lists og access keys(HTTP GET)
         """
         return json.loads(self.__http_get(f'{self.apiUrl}/access-keys/').content)
+
+
+    def new_access_key(self, name: str, usage_limit: int) -> dict:
+        """Creates a new access key
+
+        Returns:
+            dict: 201, The newly created access key(HTTP POST)
+        """
+        __key = json.loads(self.__http_post(url=f'{self.apiUrl}/access-keys/', json_data={"method": "aes-192-gcm"}).content) # create new key
+        self.__http_put(url=f'{self.apiUrl}/access-keys/{__key["id"]}/name/', data={"name": name}) # rename it
+        self.__http_put(url=f'{self.apiUrl}/access-keys/{__key["id"]}/data-limit/', json_data={"limit": {"bytes": usage_limit}}) # set usage limit
