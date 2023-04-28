de5b7de90d1b1da4b0602198f26c586119010688
shabane <m.mohamadshabane@gmail.com>
2023-04-28 17:46:55 +0330

    [feat](create new user either in outline server and db):
    	done all the todo
    	1. create the user with given name and data usage in the outline
    	   server
    	2. append the user to the local db
    	3. create a pastebin link

--- outline/views.py
+++ outline/views.py
@@ -5,6 +5,7 @@ from .serialization import LinkSerializer, ServerSerializer
 from rest_framework.response import Response
 from .core.outline import Outline
 from .models import Server, Link
+from .core.pysbin import ubuntuir, headers
 
 
 class LinkViewSet(ModelViewSet):
@@ -14,14 +15,31 @@ class LinkViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer_class = LinkSerializer(data=request.data)
         if serializer_class.is_valid():
-            # outline_server = Outline(Server.objects.get())
-            # OrderedDict([('name', 'the test'), ('max_size', 555), ('usage', 7777), ('enabled', True), ('key', 'the key'), ('exp_date', datetime.date(2023, 4, 27)), ('pastebin_link', 'lsjfl'), ('note', 'lksdjflsjdf'), ('server', <Server: me and mehdi>)])
-            #TODO: 1. get the server and crate its Outline instance
-            #TODO: 2. add the new user to outline server
-            #TODO: 3. append the data to user table in database
-            print(serializer_class.validated_data)
-        return Response(request.data)
-
+            outline_server = Outline(serializer_class.validated_data['server'].apiUrl)
+            __name = serializer_class.validated_data['name']
+            __max_usage = serializer_class.validated_data['max_size'] * 1_000_000_000
+            __key = outline_server.new_access_key(name=__name, usage_limit=__max_usage)
+            __note = serializer_class.validated_data['note']
+            __enabled = serializer_class.validated_data['enabled']
+            __expire = serializer_class.validated_data['exp_date']
+            __paste_bin_link = ubuntuir.paste(__key['accessUrl'])
+            __server = serializer_class.validated_data['server']
+            Link.objects.create(name=__name, max_size=__max_usage, key=__key['accessUrl']+f'#{__name}', note=__note, enabled=__enabled, exp_date=__expire, pastebin_link=__paste_bin_link, server=__server)
+            return Response({
+                'id': __key['id'],
+                'name': __name,
+                'max_size': __max_usage,
+                'enabled': __enabled,
+                'key': __key['accessUrl']+f'#{__name}',
+                'exp_date': serializer_class.validated_data['exp_date'],
+                'paste_bin_link': __paste_bin_link,
+                'note': __note,
+                'server': __server.name,
+            })
+        return Response({
+            'ok': False,
+            'message': serializer_class.error_messages,
+        })
 
 class ServerViewSet(ModelViewSet):
     queryset = Server.objects.all()
