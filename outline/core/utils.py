7738094ec458bade7fd2ac1f4da5bc3bb0480cc0
shabane <m.mohamadshabane@gmail.com>
2023-05-05 14:21:06 +0330

    [feat](send each link as qrcode to telegram):
    	this feature will create qrcode for each link and then
    	send that link to telegram channel or group corespounding to
    	its server.

--- outline/core/utils.py
+++ outline/core/utils.py
@@ -1,7 +1,13 @@
 """some tools are writed here
 """    
+from telegram import Bot
 
 
 def re_wrapp_domain(key: str, domain: str, port: int):
     pre_domain = key[key.find('@')+1:key.find('/?')]
     return key.replace(pre_domain, f'{domain}:{port}')
+
+
+def send_to_telegram(channel_id: str|int, caption: str, img: bytes) -> bool:    
+    bot = Bot('806045748:AAGyPYfcE7EM6C9SWlPRKuIyHbcgZsvIn2Y')
+    bot.send_document(chat_id=channel_id, caption=caption, document=img)
