a2b71ca4c72b1275fd060610d1120b8d038af151
shabane <m.mohamadshabane@gmail.com>
2023-05-13 02:43:56 +0330

    [maintain](use absolute path for exec files)

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ echo 'create direcgtory to save QRs'
 mkdir $BASE_DIR/static/
 
 echo 'migrate db'
-python3 $BASE_DIR/manage.py makemigrations
-python3 $BASE_DIR/manage.py migrate
+/usr/bin/python3 $BASE_DIR/manage.py makemigrations
+/usr/bin/python3 $BASE_DIR/manage.py migrate
 
 echo 'run the web site'
 cp -v $BASE_DIR/conf/echo.conf /etc/nginx/conf.d/
